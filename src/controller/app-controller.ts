0dccc8ac496831ed0a4d3fc9a4144b2944efee42
gabrielgollo <gabrielsantosgollo@gmail.com>
2022-08-08 14:44:54 -0300

    feat: added metrics service

--- src/controller/app-controller.ts
+++ src/controller/app-controller.ts
@@ -8,11 +8,11 @@ export default class AppController{
         try {
             const response = await FundService.processMessage(transactionId)
             logger.info('Ending App Controller..')
-            MetricsService.saveLatency(new Date().getTime() - startTime.getTime())
+            MetricsService.saveLatency(new Date().getTime() - startTime.getTime(), 'Confirmed')
             return response
         } catch (error: any) {
             if(error.shoudlAck === 'failure') logger.warn('This message needs a retry!')
-            MetricsService.saveLatency(new Date().getTime() - startTime.getTime())
+            MetricsService.saveLatency(new Date().getTime() - startTime.getTime(), error.message)
             return {
                 status: error.shoudlAck
             }
--- src/services/metrics-service.ts
+++ src/services/metrics-service.ts
@@ -1,6 +1,6 @@
 import { hostname } from 'os';
 
-const { INFLUX_HOST='', INFLUX_TOKEN='', INFLUX_ORG='', INFLUX_BUCKET='', APP_NAME=''} = process.env;
+const { INFLUX_HOST='', INFLUX_TOKEN='', INFLUX_ORG='', INFLUX_BUCKET='', APP_NAME='', INFLUX_MEASUREMENT=''} = process.env;
 console.log(INFLUX_HOST, INFLUX_TOKEN)
 //Creates a writer with "seconds" as precision
 
@@ -15,11 +15,12 @@ const writeApi = influxDB.getWriteApi(INFLUX_ORG, INFLUX_BUCKET)
 writeApi.useDefaultTags({location: hostname(), app: APP_NAME})
 
 export default class MetricsService{
-    static saveLatency(latency: number){
+    static saveLatency(latency: number, message: string){
         try{
-            const point1 = new Point('latency')
+            const point1 = new Point(INFLUX_MEASUREMENT)
             .tag('APP_NAME', APP_NAME)
-            .floatField('value', latency)
+            .floatField('latency', latency)
+            .stringField('message', message)
             
             
             writeApi.writePoint(point1)
